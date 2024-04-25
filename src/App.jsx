bf380cf4657730db19e60812d2d76d064c4be7c0
fatemeabedi <abedifateme1378@gmail.com>
2024-04-25 12:41:42 +0330

    import AutoComplete Box in App

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import AutocompleteBox from './assets/AutoCompleteBox'
+
+function App() {
+  
+  return (
+    <div className='wrapper'>
+      <AutocompleteBox/>
+    </div>
+    )
+}
+
+export default App
